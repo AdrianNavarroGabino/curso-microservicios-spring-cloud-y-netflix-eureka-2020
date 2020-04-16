17193bebed9f5f09ee8858fafd241b1002bafbe1
AdrianNavarroGabino <adriannavarrogabino@gmail.com>
2020-04-16 10:43:06 +0200

    Leer configuración desde controlador utilizando anotación @Value

--- springboot-servicio-item/src/main/java/com/adriannavarrogabino/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/adriannavarrogabino/controllers/ItemController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.adriannavarrogabino.models.Item;
 import com.adriannavarrogabino.models.Producto;
 import com.adriannavarrogabino.models.services.IItemService;
