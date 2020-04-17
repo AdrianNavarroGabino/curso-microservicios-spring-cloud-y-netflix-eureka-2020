fdf8265c2e150434d5a1125183454506e3c4fce3
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-17 09:09:50 +0200

    Creando librería commons y generando jar usando maven

---
+++ springboot-servicio-commons/src/main/java/com/adriannavarrogabino/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.adriannavarrogabino;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+}
