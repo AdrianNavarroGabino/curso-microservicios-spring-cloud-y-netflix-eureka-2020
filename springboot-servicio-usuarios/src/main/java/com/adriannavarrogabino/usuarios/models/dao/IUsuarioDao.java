11ed3fe16044056d16beb2b03c47b464a7c089de
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-17 11:43:15 +0200

    Exportar métodos repositorio rest con endpoint search

--- springboot-servicio-usuarios/src/main/java/com/adriannavarrogabino/usuarios/models/dao/IUsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/adriannavarrogabino/usuarios/models/dao/IUsuarioDao.java
@@ -2,14 +2,18 @@ package com.adriannavarrogabino.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.adriannavarrogabino.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface IUsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-	public Usuario findByUsername(String username);
+	// http://localhost:8090/api/usuarios/usuarios/search/buscar-username?nombre=adrian
+	@RestResource(path = "buscar-username")
+	public Usuario findByUsername(@Param("nombre") String username);
 	
 	@Query("select u from Usuario u where u.username = ?1")
 	public Usuario obtenerPorUsername(String username);
