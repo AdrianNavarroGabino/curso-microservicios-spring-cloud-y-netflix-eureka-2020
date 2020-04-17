e0627587b02c75d789130b315dcf803216f7d9a8
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-17 10:38:24 +0200

    Creando la relación de muchos a muchos entre Usuario y Role

--- springboot-servicio-usuarios/src/main/java/com/adriannavarrogabino/usuarios/models/entity/Usuario.java
+++ springboot-servicio-usuarios/src/main/java/com/adriannavarrogabino/usuarios/models/entity/Usuario.java
@@ -1,13 +1,20 @@
 package com.adriannavarrogabino.usuarios.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "usuarios")
@@ -31,6 +38,13 @@ public class Usuario implements Serializable {
 
 	@Column(unique = true)
 	private String email;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuarios_roles",
+		joinColumns = @JoinColumn(name = "usuario_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"),
+		uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "role_id"})})
+	private List<Role> roles;
 
 	public Long getId() {
 		return id;
@@ -86,6 +100,14 @@ public class Usuario implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
 	}
 
 	private static final long serialVersionUID = 1L;
