a8ea57fad13885fb68965842219da88a5ecc834c
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-17 08:42:04 +0200

    Consumiendo API Rest con RestTemplate desde servicio items

--- springboot-servicio-item/src/main/java/com/adriannavarrogabino/models/services/ItemServiceFeign.java
+++ springboot-servicio-item/src/main/java/com/adriannavarrogabino/models/services/ItemServiceFeign.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.adriannavarrogabino.clientes.IProductoClienteRest;
 import com.adriannavarrogabino.models.Item;
+import com.adriannavarrogabino.models.Producto;
 
 /*
  * Dos alternativas cuando hay dos clases que implementan la misma interfaz:
@@ -37,6 +38,24 @@ public class ItemServiceFeign implements IItemService {
 	public Item findById(Long id, Integer cantidad) {
 		
 		return new Item(clienteFeign.detalle(id), cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
