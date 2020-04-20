e52be7a80ddd5052acf24573e91ea352c4ab37eb
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-20 10:04:00 +0200

    Probando la autenticación con Postman y obteniendo el token JWT

--- springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/SpringbootServicioServicioOauthApplication.java
+++ springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/SpringbootServicioServicioOauthApplication.java
@@ -1,17 +1,36 @@
 package com.adriannavarrogabino.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SpringbootServicioServicioOauthApplication {
+public class SpringbootServicioServicioOauthApplication implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEnconde;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioServicioOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		String password = "12345";
+		
+		for(int i = 0; i < 4; i++)
+		{
+			System.out.println(passwordEnconde.encode(password));
+		}
+		
 	}
 
 }
