3a768b2fb7579a643bbc22b8bab6b926b6cfd205
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-20 08:48:14 +0200

    Creando nuestra interfaz Feign Client

---
+++ springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/clients/IUsuarioFeignClient.java
@@ -0,0 +1,14 @@
+package com.adriannavarrogabino.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.adriannavarrogabino.usuarios.commons.models.entity.Usuario;
+
+@FeignClient(name = "servicio-usuarios")
+public interface IUsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String username);
+}
