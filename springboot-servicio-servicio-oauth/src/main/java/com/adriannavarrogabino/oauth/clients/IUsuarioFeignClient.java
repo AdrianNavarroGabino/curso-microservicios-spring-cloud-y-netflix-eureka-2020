8e7889eae399598d0ab69ea4ae901081b8291342
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-21 08:30:28 +0200

    Implementando evento tres intentos en el login - parte 1

--- springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/clients/IUsuarioFeignClient.java
+++ springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/clients/IUsuarioFeignClient.java
@@ -2,6 +2,8 @@ package com.adriannavarrogabino.oauth.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.adriannavarrogabino.usuarios.commons.models.entity.Usuario;
@@ -11,4 +13,7 @@ public interface IUsuarioFeignClient {
 
 	@GetMapping("/usuarios/search/buscar-username")
 	public Usuario findByUsername(@RequestParam(name = "username") String username);
+	
+	@PutMapping("/usuarios/{id}")
+	public Usuario update(@RequestBody Usuario usuario, @RequestParam Long id);
 }
--- springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/security/event/AutheticationSuccessErrorHandler.java
+++ springboot-servicio-servicio-oauth/src/main/java/com/adriannavarrogabino/oauth/security/event/AutheticationSuccessErrorHandler.java
@@ -2,16 +2,22 @@ package com.adriannavarrogabino.oauth.security.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import com.adriannavarrogabino.oauth.services.IUsuarioService;
+
 @Component
 public class AutheticationSuccessErrorHandler implements AuthenticationEventPublisher {
 	
 	private Logger log = LoggerFactory.getLogger(AutheticationSuccessErrorHandler.class);
+	
+	@Autowired
+	private IUsuarioService usuarioService;
 
 	@Override
 	public void publishAuthenticationSuccess(Authentication authentication) {
